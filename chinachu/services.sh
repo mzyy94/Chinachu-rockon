eea30f3e79428e89eabbb376dc4e7e3c872af42c
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-07-04 01:28:12 +0900

    Add service stopping command for safe-shutdown

--- chinachu/services.sh
+++ chinachu/services.sh
@@ -4,4 +4,9 @@ cat /usr/local/chinachu/config.sample.json > /usr/local/chinachu/conf/config.jso
 fi
 /etc/init.d/chinachu-operator start
 /etc/init.d/chinachu-wui start
-tail -f /dev/null
+
+### failed to run with an error by `ps -p` option
+# exec bash --rcfile <(cat <<<"trap '/etc/init.d/chinachu-wui stop; /etc/init.d/chinachu-operator stop; exit 0' TERM")
+
+trap 'echo "Stopping services..."; pkill -QUIT -P $(cat /var/run/chinachu-wui.pid); pkill -QUIT -P $(cat /var/run/chinachu-operator.pid); echo done; exit 0' TERM
+while true; do sleep 1; done
