1861c3837cd3cdc20011ac1939239da726a20607
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-07-03 21:23:54 +0900

    Change chinachu config files volume store type

--- chinachu/services.sh
+++ chinachu/services.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -f /usr/local/chinachu/conf/config.json ]; then
+cat /usr/local/chinachu/config.sample.json > /usr/local/chinachu/conf/config.json
+fi
 /etc/init.d/chinachu-operator start
 /etc/init.d/chinachu-wui start
 tail -f /dev/null
